825b394142f0ab978ce5cc6468c569efcea5df40
Bampton, James (jb704y) <jb704y@intl.att.com>
2021-06-08 11:25:26 +0100

    Updates all over the place

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,15 +1,18 @@
-// Create endpoint onbject
-nlpData ={};
+/* The code is executed using npm run build - This starts server and sets up all required routes*/
+
+// Create endpoint object to hold data from POST requests
+nlpData = {};
 
 // Create varibale for hiding API key
 const dotenv = require('dotenv');
 dotenv.config();
 
 // API REFERENCES //
+//const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+//const apiKey = `${process.env.API_KEY}`; //630e7f329aa632f6fb3857e4c71f4be9
+//const language = '&lang=en&'
 
-const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
-const apiKey = `${process.env.API_KEY}`; //630e7f329aa632f6fb3857e4c71f4be9
-const language = '&lang=en&'
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 // Start Express
 const express = require('express')
@@ -30,25 +33,44 @@ const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors');
 app.use(cors());
 
+// node-fetch
+const fetch = require('node-fetch');
+
 /* SERVER SPECIFICS */
 
 // Setup deployment folder
 app.use(express.static('dist'))
 console.log(__dirname)
 
-
 app.get('/', function (req, res) {
 res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // Assign port for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on production port 8081!')
 })
 
-///////////////////////////////////
-//
-///////////////////////////////////
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+///////////////////////////////////
+// POST DATA FOR 
+///////////////////////////////////
+app.post('/nlpData', sendNLP);
+
+function sendNLP (request, response){
+  response.send(nlpData);
+};
+
+
+app.post('/nlpData', async(request, response)=>{
+
+    const apiResult = await fetch ()
+});
+
+
+
+
+//// END /////////////////////////
